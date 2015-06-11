5a153783b543a0c79c775806d73f53894dfb1d96
Jonathan Gertig <jcgertig@gmail.com>
2015-06-10 22:14:10 -0400

    add base handleing

--- app/controllers/echo/slack_controller.rb
+++ app/controllers/echo/slack_controller.rb
@@ -1,2 +1,78 @@
 class Echo::SlackController < ApplicationController
+  def handle
+    Slack.configure do |config|
+      config.token = ENV["SLACK_SECRET"]
+    end
+
+    listening = params["session"]["attributes"]["listening"]
+
+    attributes = { "listening": listening }
+
+    if listening.nil? || listening == false
+      attributes["listening"] = true
+
+      client = Slack.realtime
+
+      client.on :hello do
+        logger.info 'Successfully connected.'
+      end
+
+      client.on :message do |data|
+        render json: {
+          "version": "1.0",
+          "sessionAttributes": attributes,
+          "response": {
+            "outputSpeech": {
+              "type": "PlainText",
+              "text": data["text"]
+            },
+            "shouldEndSession": false
+          }
+        }
+      end
+
+      client.start
+    end
+
+    request = params["request"]
+    if request["type"] == "IntentRequest"
+      handleIntent(Slack, request["intent"])
+    end
+
+    render json: {
+      "version": "1.0",
+      "sessionAttributes": attributes,
+      "response": {
+        "outputSpeech": {
+          "type": "string",
+          "text": "string"
+        },
+        "card": {
+          "type": "string",
+          "title": "string",
+          "content": "string"
+        },
+        "reprompt": {
+          "outputSpeech": {
+            "type": "string",
+            "text": "string"
+          }
+        },
+        "shouldEndSession": false
+      }
+    }
+  end
+
+
+  private
+
+    def handleIntent(Slack, intent)
+      name = intent["name"]
+      message = intent["slots"]["message"]["value"]
+      if name == "SendMessage"
+        Slack.chat_postMessage text: message
+      elsif name == "SendMessageToRoom"
+        Slack.chat_postMessage channel: intent["slots"]["channelName"]["value"], text: message
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :echo do
+    scope :slack do
+      get '/', to: 'slack#handle'
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
